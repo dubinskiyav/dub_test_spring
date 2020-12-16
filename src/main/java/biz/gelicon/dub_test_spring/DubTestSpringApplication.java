6c7702ed1f69a8dd028583c1bf91d9d23e4bcec5
dav <dav@gelicon.biz>
2020-12-16 12:49:29 +0500

    Инициализация проекта

---
+++ src/main/java/biz/gelicon/dub_test_spring/DubTestSpringApplication.java
@@ -0,0 +1,13 @@
+package biz.gelicon.dub_test_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubTestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubTestSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/biz/gelicon/dub_test_spring/DubTestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.gelicon.dub_test_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubTestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
